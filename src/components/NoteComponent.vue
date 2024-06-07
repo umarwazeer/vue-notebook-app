5b978c60f19b6559053500fe98b724631ee74572
umarwazeer <a4umarkhan@gmail.com>
2024-06-07 17:20:00 +0500

    Add dist folder

--- src/components/NoteComponent.vue
+++ src/components/NoteComponent.vue
@@ -1,8 +1,28 @@
 <template>
+    <Card style="width: 25rem; overflow: hidden">
+    <template #header>
+        <img alt="user header" src="/images/usercard.png" />
+    </template>
+    <template #title>Advanced Card</template>
+    <template #subtitle>Card subtitle</template>
+    <template #content>
+        <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
+            quas!
+        </p>
+    </template>
+    <template #footer>
+        <div class="flex gap-3 mt-1">
+            <Button label="Cancel" severity="secondary" outlined class="w-full" />
+            <Button label="Save" class="w-full" />
+        </div>
+    </template>
+</Card>
         <div class="row">
             <div class="col-sm-6 note">
               <div class="card">
                 <button class="close" style="" @click="$emit('remove', index)">&times;</button>
+                <button class="close" style="" @click="$emit('edite', index)">&times;</button>
                 <div class="card-block notes">
                   <h4 class="card-title">{{note.title}}</h4>
                   <h6 class="card-subtitle mb-2 text-muted">{{note.date}}</h6>
@@ -13,7 +33,10 @@
           </div>
   </template>
   
-  <script>
+<script>
+
+import Card from 'primevue/card';
+
   export default {
     props: {
     note: {
@@ -68,7 +91,7 @@ button.btn {
 	padding: 15px 20px;
 	font-family: 'Ubuntu', sans-serif;
 	margin: 15px 0px;
-  width: 100%;
+    width: 100%;
 	box-shadow: 0px 1px 3px rgb(217, 217, 217);
 	border: none; 
 	border-radius: 2px; 
@@ -130,6 +153,10 @@ p.card-text {
     box-align: left;
     width: 1100px
 }
-
+.close{
+    float: right;
+    padding: 10px;
+    margin: 10px;
+}
 </style>
   
